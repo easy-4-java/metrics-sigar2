49a5d4c74715e817f4d41dc0bd6d277114bcca04
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-18 10:46:36 +0800

    refactor: groupId io.github.hiwepy -> io.github.easy4j, package rename

--- src/main/java/com/codahale/metrics/sigar/utils/CapacityUnit.java
+++ src/main/java/com/codahale/metrics/sigar/utils/CapacityUnit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 (https://github.com/hiwepy).
+ * Copyright (c) 2018 (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -17,7 +17,7 @@ package com.codahale.metrics.sigar.utils;
 
 /**
  * http://www.mamicode.com/info-detail-1068774.html
- * @author <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author <a href="https://github.com/easy-4-java">hiwepy</a>
  */
 public enum CapacityUnit {
 	
--- src/main/java/com/codahale/metrics/sigar/utils/OSEnvInfo.java
+++ src/main/java/com/codahale/metrics/sigar/utils/OSEnvInfo.java
@@ -175,7 +175,7 @@ public class OSEnvInfo {
 	 *	st：虚拟机偷取时间
 	 * 
 	 * @description	： TODO
-	 * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author 		： <a href="https://github.com/easy-4-java">hiwepy</a>
 	 * @date 		：2017年6月20日 下午5:28:55
 	 * @param sigar
 	 * @return
